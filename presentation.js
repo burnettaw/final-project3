075bd2723a88bba078e180c568a84d6ee9d4285c
Burnetta Wood <burnettawood@gmail.com>
2021-08-16 14:24:43 -0400

    uploading files

--- presentation.js
+++ presentation.js
@@ -1,19 +1,19 @@
 var tbody = d3.select("#resultTable")
-
+​
 tableData = [];
-
+​
 d3.csv("pollutants_mean.csv").then(function (data) {
     tableData = data
     console.log("data")
     console.log(data)
 });
-
+​
 function optionChanged(selOption) {
     console.log("selOption")
     console.log(selOption)
-    
+​
     tbody.html("");
-    
+​
     const tbl_header = tbody.append("tr");
     let header = tbl_header.append("th");
     header.text("\xa0\xa0 City \xa0\xa0");
@@ -25,10 +25,10 @@ function optionChanged(selOption) {
     header.text("\xa0 SO2 AQI \xa0");
     header = tbl_header.append("th");
     header.text("\xa0 CO AQI \xa0");
-    
+​
     label_list = [];
     data_list = [];
-    
+​
     for (var i = 0; i < 3; i++) {
         let tbl_row = tbody.append("tr");
         let cell = tbl_row.append("td");
@@ -42,7 +42,7 @@ function optionChanged(selOption) {
         cell = tbl_row.append("td");
         cell.text(tableData[i]["CO"]);
     }
-    
+​
     if (selOption == "Los Angeles") {
         i = 0
     }
@@ -52,7 +52,7 @@ function optionChanged(selOption) {
     else if (selOption == "Raleigh") {
         i = 2
     }
-    
+​
     label_list.push("NO2")
     data_list.push(tableData[i]["NO2"]);
     label_list.push("Ozone")
@@ -61,7 +61,7 @@ function optionChanged(selOption) {
     data_list.push(tableData[i]["SO2"]);
     label_list.push("CO")
     data_list.push(tableData[i]["CO"]);
-    
+​
     //  doughnut  
     new Chart(document.getElementById("doughnut-chart"), {
         type: 'doughnut',
@@ -81,6 +81,7 @@ function optionChanged(selOption) {
                 text: 'Pollutant Percentages in ' + tableData[i]["City"]
             }
         }
+​
     });
-
+​
 }
